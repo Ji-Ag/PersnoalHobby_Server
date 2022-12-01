252d933e68727ec33016de4d564758a18c5b0c94
Ji-Ag <karajuliason@naver.com>
2022-12-01 21:57:22 +0900

    fix :cors

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 var router = express.Router();
 const cors = require('cors');
 
+var app = express();
+
 app.use(cors({
   origin:"http://localhost:3000",
   methods: ["GET","POST"],
 }
 ));
-var app = express();
+
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
