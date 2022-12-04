774753683d058226df55af3a2eb14535527cf071
Ji-Ag <karajuliason@naver.com>
2022-12-04 15:55:29 +0900

    database바꿈

--- mysql/index.js
+++ mysql/index.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
     connectionLimit :  10,//process.env.LIMIT, 
-    host : 'firstserver.ctk6srnbe3rf.us-east-1.rds.amazonaws.com',//process.env.HOST, 
+    host : 'personalhobby.ctk6srnbe3rf.us-east-1.rds.amazonaws.com',//process.env.HOST, 
     port : '3306',//process.env.PORT, 
     user : 'admin',//process.env.DB_USER, 
     password : 'mysqlqlqjs1',//process.env.PASSWORD, 
-    database :  "firstserver"
+    database :  "personalhobby"
 });
 
 const query = async (alias,values)=>{
